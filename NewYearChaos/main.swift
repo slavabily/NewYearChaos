b629290372dd8ce25007314df3f3c02474721242
slava bily <slavabily@me.com>
2021-11-24 22:10:46 +0200

    first option of flow for solving task

--- NewYearChaos/main.swift
+++ NewYearChaos/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+func minimumBribes(q: [Int]) -> Void {
+    let t = q
+    var bribes = Int()
+    for i in 0..<t.count - 1 {
+        if t[i] - t[i + 1] > 2 {
+            print("Too chaotic")
+            return
+        } else if t[i] - t[i + 1] == 1 {
+            bribes += 1
+        } else if t[i] - t[i + 1] == 2 {
+            bribes += 2
+        }
+    }
+    print(bribes)
+}
+
+minimumBribes(q: [4,3,2,1])
 
