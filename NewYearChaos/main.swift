edba008e8f39d9a3bc28d1e91192a70709b601b4
slava bily <slavabily@me.com>
2021-11-25 10:27:16 +0200

    another algorithm, comparing array's indices with its values with some bugs

--- NewYearChaos/main.swift
+++ NewYearChaos/main.swift
@@ -9,19 +9,20 @@ import Foundation
 
 func minimumBribes(q: [Int]) -> Void {
     let t = q
-    var bribes = Int()
-    for i in 0..<t.count - 1 {
-        if t[i] - t[i + 1] > 2 {
+    var bribes = 0
+    for (index, item) in t.enumerated() {
+//        print("Index: \(index), Item: \(item)")
+        if item - (index + 1) > 2 {
             print("Too chaotic")
             return
-        } else if t[i] - t[i + 1] == 1 {
+        } else if item - index + 1 == 1 {
             bribes += 1
-        } else if t[i] - t[i + 1] == 2 {
+        } else if item - index + 1 == 2 {
             bribes += 2
         }
     }
     print(bribes)
 }
 
-minimumBribes(q: [4,3,2,1])
+minimumBribes(q: [1, 2, 5, 3, 4, 7, 8, 6])
 
